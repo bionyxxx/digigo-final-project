946d563b254058d396fe653106e1939d821dc19d
Dutta Fachrezy <duttafachrezy2002@gmail.com>
2023-04-12 17:42:44 +0700

    migration to repository pattern

--- app.go
+++ app.go
@@ -8,8 +8,21 @@ import (
 	"Final_Project/services"
 	"fmt"
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 	"os"
 )
+
+func InitApp() {
+	err := godotenv.Load()
+	if err != nil {
+		panic(err)
+	}
+
+	err = configs.InitDatabase()
+	if err != nil {
+		panic(err)
+	}
+}
 
 func StartApp() {
 	var router = gin.New()
--- main.go
+++ main.go
@@ -1,20 +1,7 @@
 package main
 
-import (
-	"Final_Project/configs"
-	"github.com/joho/godotenv"
-)
-
 func init() {
-	err := godotenv.Load()
-	if err != nil {
-		panic(err)
-	}
-
-	err = configs.InitDatabase()
-	if err != nil {
-		panic(err)
-	}
+	InitApp()
 }
 
 func main() {
